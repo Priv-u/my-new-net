a81400da07d2af7edd2ad42ec28c3d2ed091112c
Priv-u <priv-u@mail.ru>
2022-05-04 23:26:43 +0600

    Добавлена иконка самолета в Hesder

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,7 +4,7 @@ import s from './Header.module.css';
 const Header = () => {
   return (
     <div className={s.appHeader}>
-
+      <img src={require('./../../images/airplane.png')} alt="иконка самолета" />
       Header
     </div>
   );
