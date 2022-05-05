182dd32e5fdd3e7f9682a2e266bfe9a27a4a4827
Priv-u <priv-u@mail.ru>
2022-05-05 16:36:37 +0600

    Добавление картинки в основной блок

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Header from './components/Header/Header';
 const Profile = () => {
   return (
     <div>
-      <img src='' alt='Красивый пейзаж' />
+      <img src={require('./images/mountains.jpg')} alt='Красивый пейзаж' />
       Новые сообщения, которые будут добавляться на ленту.
       Основной профиль
     </div>
