6a18fef5e18cc40df75ef83ab8dde83ae1909214
Priv-u <priv-u@mail.ru>
2022-05-18 01:35:20 +0600

    Просто очистил код от лишних закомментированных фрагментов

--- src/components/Dialogs/Messages/MessagesContainer.jsx
+++ src/components/Dialogs/Messages/MessagesContainer.jsx
@@ -1,29 +1,7 @@
-// import React from "react";
 import { connect } from "react-redux";
 import { addMessageActionCreator, updateNewMessageActionCreator } from './../../../redux/dialog-reducer'
 import Messages from './Messages';
 
-
-// const MessagesContainer = (props) => {
-
-//   let state = props.store.getState();
-//   // TODO Просмотреть видео до 48 и переписать компоненты с помощью 
-//   // ract-redux и connet 
-
-//   let addMessage = () => {
-//     props.store.dispatch(addMessageActionCreator());
-//   }
-
-//   let onMessageChange = (text) => {
-//     props.store.dispatch(updateNewMessageActionCreator(text));
-//   }
-//   return (
-//     <Messages addMessage={addMessage}
-//       updateNewMessageText={onMessageChange}
-//       messages={state.dialogsPage.messages}
-//       newMessage={state.dialogsPage.newMessage} />
-//   )
-// }
 
 let mapStateToProps = (state) => {
   return {
--- src/index.js
+++ src/index.js
@@ -12,7 +12,6 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 // При передаче  методов через пропсы необходимо эти 
 // методы забиндить на владельца т.е. store
 
-// let rerenderEntireTree = () => {
 root.render(
 
   <React.StrictMode>
@@ -22,17 +21,5 @@ root.render(
 
   </React.StrictMode>
 );
-// }
-
-// rerenderEntireTree(store.getState());
-
-// // При использовании методов нет тебходимости биндить метод к владельцу, т.к. вызов метода сразу
-// // выполняется от его владельца.
-// store.subscribe(() => {
-//   let state = store.getState();
-//   rerenderEntireTree(state);
-// }
-// );
 
 reportWebVitals();
-//TODO Проработать активность кнопок в Навбаре
