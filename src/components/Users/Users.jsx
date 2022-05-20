745bbfa9b4755040985ba7bb3fffa6402df8123e
Priv-u <priv-u@mail.ru>
2022-05-20 15:09:26 +0600

    Отработано первое обращение к серверу для получения данных для ветки users

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,46 +1,55 @@
+import axios from "axios";
 import React from "react";
 import s from './Users.module.css';
+import UserImage from './../../images/user.png'
 
 let Users = (props) => {
   // Проверка на наличие объектов в массиве пользователей выполняется в users-reducer
+  if (props.users.length === 0) {
+    axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
+      props.setUsers(response.data.items)
+    });
+    /*props.setUsers(
+      [
+      {
+        id: 1, fotoUrl: 'https://mycomp.su/wp-content/uploads/zachem-nuzhen-avatar.jpg',
+        followed: true, userName: 'Алена',
+        status: 'В постоянном переживании', location: { country: 'Казахстан', city: 'Алматы' }
+      },
+      {
+        id: 2, fotoUrl: 'https://mycomp.su/wp-content/uploads/zachem-nuzhen-avatar.jpg',
+        followed: true, userName: 'Алеся',
+        status: 'Всегда активна', location: { country: 'Казахстан', city: 'Алматы' }
+      },
+      {
+        id: 3, fotoUrl: 'https://mycomp.su/wp-content/uploads/zachem-nuzhen-avatar.jpg',
+        followed: false, userName: 'Алия',
+        status: 'Тихо но однозначно', location: { country: 'Казахстан', city: 'Алматы' }
+      },
+      {
+        id: 4, fotoUrl: 'https://mycomp.su/wp-content/uploads/zachem-nuzhen-avatar.jpg',
+        followed: false, userName: 'Дима',
+        status: 'Кто в армии служил, тот в цирке не смеется', location: { country: 'Казахстан', city: 'Алматы' }
+      },
+      {
+        id: 5, fotoUrl: 'https://mycomp.su/wp-content/uploads/zachem-nuzhen-avatar.jpg',
+        followed: false, userName: 'Илья',
+        status: 'Учусь, который год... ', location: { country: 'Россия', city: 'Москва' }
+      },
+      {
+        id: 6, fotoUrl: 'https://mycomp.su/wp-content/uploads/zachem-nuzhen-avatar.jpg',
+        followed: false, userName: 'Мария Юрьевна',
+        status: 'Опыт - дело нажвное! Его не пропьешь', location: { country: 'Казахстан', city: 'Алматы' }
+      },
+      {
+        id: 7, fotoUrl: 'https://mycomp.su/wp-content/uploads/zachem-nuzhen-avatar.jpg',
+        followed: false, userName: 'Азамат',
+        status: 'Хочу посмотреть весь мир', location: { country: 'Казахстан', city: 'Алматы' }
+      }
+    ]
+    );*/
+  }
 
-  props.setUsers([
-    {
-      id: 1, fotoUrl: 'https://mycomp.su/wp-content/uploads/zachem-nuzhen-avatar.jpg',
-      followed: true, userName: 'Алена',
-      status: 'В постоянном переживании', location: { country: 'Казахстан', city: 'Алматы' }
-    },
-    {
-      id: 2, fotoUrl: 'https://mycomp.su/wp-content/uploads/zachem-nuzhen-avatar.jpg',
-      followed: true, userName: 'Алеся',
-      status: 'Всегда активна', location: { country: 'Казахстан', city: 'Алматы' }
-    },
-    {
-      id: 3, fotoUrl: 'https://mycomp.su/wp-content/uploads/zachem-nuzhen-avatar.jpg',
-      followed: false, userName: 'Алия',
-      status: 'Тихо но однозначно', location: { country: 'Казахстан', city: 'Алматы' }
-    },
-    {
-      id: 4, fotoUrl: 'https://mycomp.su/wp-content/uploads/zachem-nuzhen-avatar.jpg',
-      followed: false, userName: 'Дима',
-      status: 'Кто в армии служил, тот в цирке не смеется', location: { country: 'Казахстан', city: 'Алматы' }
-    },
-    {
-      id: 5, fotoUrl: 'https://mycomp.su/wp-content/uploads/zachem-nuzhen-avatar.jpg',
-      followed: false, userName: 'Илья',
-      status: 'Учусь, который год... ', location: { country: 'Россия', city: 'Москва' }
-    },
-    {
-      id: 6, fotoUrl: 'https://mycomp.su/wp-content/uploads/zachem-nuzhen-avatar.jpg',
-      followed: false, userName: 'Мария Юрьевна',
-      status: 'Опыт - дело нажвное! Его не пропьешь', location: { country: 'Казахстан', city: 'Алматы' }
-    },
-    {
-      id: 7, fotoUrl: 'https://mycomp.su/wp-content/uploads/zachem-nuzhen-avatar.jpg',
-      followed: false, userName: 'Азамат',
-      status: 'Хочу посмотреть весь мир', location: { country: 'Казахстан', city: 'Алматы' }
-    }
-  ]);
 
   return (
     <div className={s.usersWrapper}>
@@ -49,7 +58,7 @@ let Users = (props) => {
         props.users.map(u => <div key={u.id} className={s.user}>
           <div className={s.avaAndFollow}>
             <div className={s.ava}>
-              <img src={u.fotoUrl} alt='фото' />
+              <img src={u.photos.small != null ? u.photos.small : UserImage} alt='фото' />
             </div>
 
             <div className={s.followButton}>
@@ -61,7 +70,7 @@ let Users = (props) => {
 
           <div className={s.mainInfo}>
             <div className={s.name}>
-              {u.userName}
+              {u.name}
             </div>
 
             <div className={s.status}>
@@ -69,11 +78,11 @@ let Users = (props) => {
             </div>
 
             <div className={s.country}>
-              {u.location.country}
+              {"u.location.country - нет на сервере"}
             </div>
 
             <div className={s.city}>
-              {u.location.city}
+              {"u.location.city - нет на сервере"}
             </div>
           </div>
 
