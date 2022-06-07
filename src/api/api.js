598bd0328ca69e75cccc27a397f1b35cd54d0445
Priv-u <priv-u@mail.ru>
2022-06-07 21:13:32 +0600

    использование локального стейта

--- src/api/api.js
+++ src/api/api.js
@@ -20,12 +20,21 @@ export const usersAPI = {
 }
 
 export const profileAPI = {
+
   getProfile(userId) {
     return (
       instance.get(`profile/${userId}`)
         .then(response => response.data)
     )
+  },
+  // FIXME Подготовил API для получения статуса
+  getProfileStatus(userId) {
+    return (
+      instance.get(`profile/status${userId}`)
+        .then(response => response.data)
+    )
   }
+
 }
 
 export const authAPI = {
